149ff610d1c0be212f1f9b67d2d499509a80031d
Roman <rallyshops@gmail.com>
2022-07-29 00:16:41 +0300

    connected database, refactor app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('hello API!')
+require('dotenv/config');
+
+const api = process.env.API_URL;
+const productsRouter = require('./routers/products');
+
+//Middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+
+//Routers
+app.use(`${api}/products`, productsRouter)
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
+  .then(() => console.log('Database Connection is ready...'))
+  .catch(err => console.log(err))
 
 app.listen(3000, () => {
+  console.log(api);
+
   console.log('server is running http://localhost:3000');
 })
 
